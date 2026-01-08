4da5eda5ea5eb4c9822a59ffccd6f81880fdfb22
Danilo Campos <danilo@posthog.com>
2026-01-08 16:25:18 -0500

    SvelteKit example

---
+++ basics/sveltekit/src/lib/components/Header.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import { getAuthContext } from '$lib/auth.svelte';
+
+	const auth = getAuthContext();
+</script>
+
+<header class="header">
+	<div class="header-container">
+		<nav>
+			<a href="/">Home</a>
+			{#if auth.user}
+				<a href="/burrito">Burrito</a>
+				<a href="/profile">Profile</a>
+			{/if}
+		</nav>
+		<div class="user-section">
+			{#if auth.user}
+				<span>Welcome, {auth.user.username}</span>
+				<button class="btn-logout" onclick={() => auth.logout()}>Logout</button>
+			{/if}
+		</div>
+	</div>
+</header>
