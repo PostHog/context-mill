c82dd537375cf407c433bf718fbd30befca30a65
Vincent (Wen Yu) Ge <gewenyu99@gmail.com>
2026-02-06 15:04:01 -0500

    Okay add other skip file patterns

---
+++ basics/android/app/build/generated/source/buildConfig/debug/com/example/posthog/BuildConfig.java
@@ -0,0 +1,16 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.example.posthog;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.example.posthog";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+  // Field from default config.
+  public static final String POSTHOG_API_KEY = "phc_a1NMvM9GG5oB2mO2CSdyIDavyrHz1qB2YgYuIEHRgJc";
+  // Field from default config.
+  public static final String POSTHOG_HOST = "https://us.i.posthog.com";
+}
