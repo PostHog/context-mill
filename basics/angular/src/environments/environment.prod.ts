e1414c4cc7a8aeddda9f46ad8d5c8920ac7a1fdf
Edwin Lim <edwin@posthog.com>
2026-02-09 14:45:04 -0500

    ng_ env var

--- basics/angular/src/environments/environment.prod.ts
+++ basics/angular/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  posthogKey: import.meta.env.VITE_POSTHOG_KEY ?? '<ph_project_api_key>',
-  posthogHost: import.meta.env.VITE_POSTHOG_HOST ?? 'https://us.posthog.com',
+  posthogKey: import.meta.env['NG_APP_POSTHOG_KEY'] || '<ph_project_api_key>',
+  posthogHost: import.meta.env['NG_APP_POSTHOG_HOST'] || 'https://us.posthog.com',
 };
